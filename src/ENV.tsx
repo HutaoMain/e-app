72dfeaacbc7c8b17523436821a80ea8805882ded
HutaoMain <alimohamedalcantara@gmail.com>
2023-11-26 08:52:40 +0800

    add ! in the firebase

---
+++ src/ENV.tsx
@@ -0,0 +1,6 @@
+export const apiKey = "AIzaSyCMEfJu0_q1k7bhXDLw1soOtreCGVAJaTI";
+export const authDomain = "e-book-app-e8252.firebaseapp.com";
+export const projectId = "e-book-app-e8252";
+export const storageBucket = "e-book-app-e8252.appspot.com";
+export const messagingSenderId = "811746581459";
+export const appId = "1:811746581459:web:fcad008f9e663e66c42d26";
